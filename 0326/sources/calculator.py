5080096a9e7c5b6962f80a19c8928add363d2948
chuang0709 <chunang0709@gmail.com>
2018-03-29 13:46:07 +0800

    0326

---
+++ 0326/sources/calculator.py
@@ -0,0 +1,10 @@
+def cal(x,y,m):
+    if m=="+":  
+        return x+y
+    elif m=="-":
+        return x-y
+    elif m=="*":
+        return x*y
+        print(x*y)
+    elif m=="/":
+        return x/y
