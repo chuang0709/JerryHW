1eea6b5dbc948310f8884674b862a5cbf982bf68
chuang0709 <chunang0709@gmail.com>
2018-05-04 23:15:15 +0800

    0430

---
+++ 0430/ntust/mysite/mothersday/migrations/0001_initial.py
@@ -0,0 +1,19 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Mother',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('content', models.CharField(max_length=1000)),
+            ],
+        ),
+    ]
